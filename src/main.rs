68230581b32cd14f146f000f0d8049bc472bb9b1
usagiga <usagiga@usagiga.me>
2025-02-20 13:05:20 +0900

    feat: hello world using nom

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use nom::character::complete::char;
+use nom::sequence::delimited;
+use nom::{IResult, Parser};
+use nom::bytes::complete::take_while;
+
+fn parse_json(input: &str) -> IResult<&str, &str> {
+    // パース
+    let mut parser = delimited(
+        char('{'),
+        take_while(|c| c != '}'),
+        char('}'),
+    );
+
+    parser.parse(input)
+}
+
 fn main() {
-    println!("Hello, world!");
+    // 空のJSON文字列
+    let input = "{\"hoge\": \"fuga\"}";
+    // パース
+    let parsed = parse_json(&input);
+    println!("Parsed JSON: {:?}", parsed);
 }
